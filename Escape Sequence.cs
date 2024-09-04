c940417dc42ee8d79f7cd0432e2c6fbae0a9e790
Watermelon <ejemmanuel45@gmail.com>
2024-09-04 13:20:15 +0800

    Create Escape Sequence.cs

---
+++ Escape Sequence.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\"this is a string\""); // Uses Escape Character "\"
+        // Uses Escape Character "\" inside of its C:\Windows\System32
+        Console.WriteLine("You should not delete C:\\Windows\\System32"); 
+        Console.WriteLine("Hello \t\n\t Classmates!"); // Uses Escape Sequence \t - Tab, \n - End,
+    }
+}
