3b30c51cd7f33451e557724b9c9f302fe7a7772b
Watermelon <ejemmanuel45@gmail.com>
2024-09-06 15:43:52 +0800

    Update Escape Sequence.cs

--- Escape Sequence.cs
+++ Escape Sequence.cs
@@ -6,7 +6,7 @@ public class Program
     {
         // Uses Escape Sequence: "\" - Used to include double quotes within the string.
         Console.WriteLine("Strings should be enclosed in quotation marks like so \"this is a string\"");
-        // Uses Escape Sequence: "\" - Used to include a backslash in the path. Inside of its C:\Windows\System32
+        // Uses Escape Sequence: "\\" - Used to include a backslash in the path. Inside of its C:\Windows\System32
         Console.WriteLine("You should not delete C:\\Windows\\System32");
         // Uses Escape Sequence: \t - Tab, \n - End
         Console.WriteLine("Hello \t\n\t Classmates!"); 
