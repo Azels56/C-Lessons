67457bfb046c1bf8ba16b434ec0905127c27f0b5
Watermelon <ejemmanuel45@gmail.com>
2024-09-06 10:06:41 +0800

    Update Comparing Number.cs
    Just added some comments for the horrible code of using what if else. This could've been better if for loops with arrays were utilized in this scenario. 
    
    For ex. for (int i = 0; i < sets.Length; i++) then determine the biggest, smallest with that.
    
    Or another example using method/function
    
    Make the code cleaner with that.
    
    For ex. 
    
    static void FindBigSmallNumber(int a, int b, int c)
    
    Then, put the rest of the what if horrid code there.
    Using the initialized variable inside the method.
    
    "Do not overcomplicate everything, Be simplistic" - Emmanuel

--- Comparing Number.cs
+++ Comparing Number.cs
@@ -4,26 +4,32 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // 1st set of varibles 
+        // 1st set of numbers
         int FirstSet_1stNumber = 10;
         int FirstSet_2ndNumber = 101;
         int FirstSet_3rdNumber = 11;
 
-        // 2nd set of varibles 
+        // 2nd set of numbers
         int SecondSet_1stNumber = 23;
         int SecondSet_2ndNumber = 3;
         int SecondSet_3rdNumber = 12;
 
-        // 3rd set of varibles 
+        // 3rd set of numbers
         int ThirdSet_1stNumber = 9;
         int ThirdSet_2ndNumber = 11;
         int ThirdSet_3rdNumber = 24;
 
-        // Biggest, Smallest variables
-        int biggestFirstSet = 0; int smallestFirstSet = 0;
-        int biggestSecondSet = 0; int smallestSecondSet = 0;
-        int biggestThirdSet = 0; int smallestThirdSet = 0;
+        // Biggest, Smallest variables: Used to store the biggest and smallest numbers from the 1st, 2nd and 3rd set of numbers.
+        int biggestFirstSet = 0; 
+        int smallestFirstSet = 0;
        
+        int biggestSecondSet = 0; 
+        int smallestSecondSet = 0;
+        
+        int biggestThirdSet = 0; 
+        int smallestThirdSet = 0;
+
+        // Finding biggest and smallest for the first set
         // Checks every set of number if they are greater than or equal to their opposing number)
         if (FirstSet_1stNumber >= FirstSet_2ndNumber && FirstSet_1stNumber >= FirstSet_3rdNumber) // Checks if the first number is greater or equal to than 2nd, 3rd number. 
         {
@@ -57,10 +63,11 @@ public class Program
             //Then assign the 3rd number (if smallest) to the smallest variable in their set of order
             smallestFirstSet = FirstSet_3rdNumber;
         }
-
+        // Output for the first set
         Console.WriteLine("First Set:\nBiggest Number: " + biggestFirstSet + "\nSmallest Number:" + smallestFirstSet); // Outputs the biggest and smallest
         Console.WriteLine();
 
+        // Finding biggest and smallest for the second set
         // Second Set of Order (Same explanation as previously stated)
         if (SecondSet_1stNumber >= SecondSet_2ndNumber && SecondSet_1stNumber >= SecondSet_3rdNumber)
         {
@@ -87,10 +94,11 @@ public class Program
         {
             smallestSecondSet = SecondSet_3rdNumber;
         }
-
+        // Output for the second set
         Console.WriteLine("Second Set:\nBiggest Number: " + biggestSecondSet + "\nSmallest Number:" + smallestSecondSet);
         Console.WriteLine();
 
+        // Finding biggest and smallest for the third set
         // Third Set of Order (Same explanation as previously stated)
         if (ThirdSet_1stNumber >= ThirdSet_2ndNumber && ThirdSet_1stNumber >= ThirdSet_3rdNumber)
         {
@@ -117,7 +125,7 @@ public class Program
         {
             smallestThirdSet = ThirdSet_3rdNumber;
         }
-
+        // Output for the third set
         Console.WriteLine("Third Set:\nBiggest Number: " + biggestThirdSet + "\nSmallest Number:" + smallestThirdSet);
     }
 }
