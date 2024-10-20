b45f17748a4192c150cf20686d1d1eba36ea969d
Watermelon <ejemmanuel45@gmail.com>
2024-10-20 12:16:31 +0800

    Update ChooseYourShape.cs
    new

--- ChooseYourShape.cs
+++ ChooseYourShape.cs
@@ -1,33 +1,34 @@
-
 class Program
 {
     static void Main()
-    {
+        {
         bool continueProgram = true;
-
         while (continueProgram)
         {
-
             Console.WriteLine("1. Rectangle");
-            Console.WriteLine("2. Right-Angle Triangle");
+            Console.WriteLine("2. Triangle");
             Console.WriteLine("3. Pyramid");
-            Console.WriteLine("4. Diamond");
-            Console.WriteLine("5. Exit");
-            Console.Write("Choose a shape to print: ");
-            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("4. Exit");
+            Console.Write("Choose your shape: ");
+            int selection = 0;
+            string choice = Console.ReadLine();
+            
+            if(!int.TryParse(choice, out selection))
+            {
+                Console.WriteLine("Invalid Input. Please Try Again.");
+                continue;
+            }
 
-            switch (choice)
+            switch(selection)
             {
                 case 1:
-                    Console.WriteLine("Enter the length of the rectangle:");
+                    Console.Write("Input your length: ");
                     int rectanglelength = int.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Enter the width of the rectangle:");
+                    Console.Write("Input your width: ");
                     int rectanglewidth = int.Parse(Console.ReadLine());
-
-                    for (int i = 0; i < rectanglelength; i++) // For loop for length
+                    for (int i = 0; i < rectanglelength; i++)
                     {
-                        for (int j = 0; j < rectanglewidth; j++) // For loop for width
+                        for (int j = 0; j < rectanglewidth; j++)
                         {
                             Console.Write("* ");
                         }
@@ -35,12 +36,10 @@ class Program
                     }
                     break;
                 case 2:
-                    Console.WriteLine("Enter the height of the right-angle triangle:");
                     int triangleheight = int.Parse(Console.ReadLine());
-
-                    for (int i = 1; i <= triangleheight; i++)
+                    for (int i = 0; i <= triangleheight; i++)
                     {
-                        for (int j = 1; j <= i; j++)
+                        for (int j = i; j < triangleheight; j++)
                         {
                             Console.Write("* ");
                         }
@@ -51,13 +50,13 @@ class Program
                     Console.WriteLine("Enter the height of the pyramid:");
                     int pyramidheight = int.Parse(Console.ReadLine());
 
-                    for (int i = 1; i <= pyramidheight; i++)
+                    for (int i = pyramidheight; i >= 1; i--)
                     {
-                        for (int j = i; j < pyramidheight; j++)
+                        for (int j = pyramidheight; j > i; j--)
                         {
-                            Console.Write("  ");
+                            Console.Write(" ");
                         }
-                        for (int k = 1; k <= (2 * i - 1); k++)
+                        for (int k = 1; k <= i; k++)
                         {
                             Console.Write("* ");
                         }
@@ -65,50 +64,19 @@ class Program
                     }
                     break;
                 case 4:
-                    Console.WriteLine("Enter the height of the diamond:");
-                    int diamondheight = int.Parse(Console.ReadLine());
-
-                    for (int i = 0; i <= diamondheight; i++)
-                    {
-                        for (int j = diamondheight; j > i; j--)
-                        {
-                            Console.Write(" ");
-                        }
-                        for (int k = 1; k <= i; k++)
-                        {
-                            Console.Write("* ");
-                        }
-                        Console.WriteLine();
-                    }
-                    for (int i = diamondheight - 1; i >= 1; i--)
-                    {
-                        for (int j = diamondheight; j>i; j--)
-                        {
-                            Console.Write(" ");
-                        }
-                        for (int k = 1; k <= i; k++)
-                        {
-                            Console.Write("* ");
-                        }
-                        Console.WriteLine();
-                    }
-                    break;
-                case 5:
                     continueProgram = false;
                     break;
                 default:
-                    Console.WriteLine("Invalid choice. Please choose again.");
-                    break;
+                    Console.WriteLine("Invalid Selection. Please Try Again.");
+                    continue;   
             }
 
-            if (choice != 5)
+            if(selection != 4)
             {
-                Console.WriteLine("Would you like to choose another shape? (yes/no)");
+                Console.WriteLine("Would you like to exit the program? (yes/no): ");
                 string response = Console.ReadLine().ToLower();
-                continueProgram = response == "yes";
+                continueProgram = response == "yes" == false;
             }
         }
     }
-
-
 }
