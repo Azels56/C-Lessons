6ad95fdfac7764b63e6112b34fd788c7ebc56d2f
Watermelon <ejemmanuel45@gmail.com>
2024-11-03 16:02:22 +0800

    Create MathMethods.cs

---
+++ MathMethods/MathMethods.cs
@@ -0,0 +1,25 @@
+namespace Arithmetic
+{
+    class MathMethods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
