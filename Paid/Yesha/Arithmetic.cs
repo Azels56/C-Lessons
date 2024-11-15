0de7b3e9fcd08174ab0cf33427bb5eba0cce7541
Watermelon <ejemmanuel45@gmail.com>
2024-11-15 23:52:25 +0800

    Create Arithmetic.cs

---
+++ Paid/Yesha/Arithmetic.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Arithmetic
+{
+    // This method adds two integers
+    public static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    // This method subtracts the second integer from the first
+    public static int Sub(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    // This method multiplies two integers
+    public static int Mul(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    // This method divides the first integer by the second
+    public static int Div(int num1, int num2)
+    {
+        return num1 / num2;
+    }
+}
+
