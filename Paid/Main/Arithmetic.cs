f0bfe17a3d176ffde718a0cc36ce3488a697e04e
Watermelon <ejemmanuel45@gmail.com>
2024-11-17 01:36:31 +0800

    Create Arithmetic.cs

---
+++ Paid/Main/Arithmetic.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Arithmetic
+{
+    // This method adds two integers
+    public static int Addition(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    // This method subtracts the second integer from the first
+    public static int Subtract(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    // This method multiplies two integers
+    public static int Multiply(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    // This method divides the first integer by the second
+    public static int Division(int num1, int num2)
+    {
+        return num1 / num2;
+    }
+}
+
